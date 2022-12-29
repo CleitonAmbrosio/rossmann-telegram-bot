d21d64d22ea11a0b09564e5f97f11934df109316
CleitonAmbrosio <cleiton.o.ambrosio@gmail.com>
2022-12-29 13:14:27 -0300

    Add msg reply

--- debug.py
+++ debug.py
@@ -18,18 +18,26 @@ TOKEN = os.environ.get('TOKEN')
 # setWebhook
 # https://api.telegram.org/TOKEN/setWebhook?url=<URL>
 
-def parse_message( message ):
+def parse_message(message):
     chat_id = message['message']['chat']['id']
     store_id = message['message']['text']
     
-    store_id = store_id.replace( '/',' ' )
+    store_id = store_id.replace('/', ' ')
     
     try:
-        store_id = int( store_id )
+        store_id = int(store_id)
     except ValueError:
         store_id = 'error'
     
     return chat_id, store_id
+
+
+def send_message(chat_id, text):
+    url = 'https://api.telegram.org/bot{}/sendMessage?chat_id={}'.format(TOKEN, chat_id)
+    r = requests.post(url, json={'text': text})
+    f'Status code: {r.status_code}'
+    
+    return None
 
 ######################################
 
@@ -41,7 +49,13 @@ def index():
         message = request.get_json()
         app.logger.info('Received message: %s', message)
         chat_id, store_id = parse_message(message)
-        return Response( 'Ok', status=200 ) 
+
+        if store_id != 'error':
+            send_message(chat_id, 'Store ok')
+            return Response('Ok', status=200)
+        else:
+            send_message(chat_id, 'Store not available')
+            return Response('Ok', status=200) 
         
     else:
         app.logger.info('<h1> Rossmann Telegram Bot awaiting call. </h1>')
